875f78cc36e7ccdddd48bdca1288c156e12150b3
Yuriy Sogokon <ysogokon@msn.com>
2021-09-30 11:09:54 -0400

    End of lesson 4
    Signed-off-by: Yuriy Sogokon <ysogokon@msn.com>

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -9,28 +9,28 @@ contract SimpleStorage {
     // int256 favInt = -5;   // signed int
     // address favAddress = 0x015Eb143584DfdD38D4605c73D601F41d02525Cf;
     // bytes32 favBytes = 'cat';
-    
-    
+
     // this will initialize to 0
     uint256 favNumber;
-    
+
     struct People {
         uint256 favNumber;
         string name;
     }
-    
+
     People[] public people;
     mapping(string => uint256) public nameToFavNumber;
-    
-    function store (uint256 _favNumber) public {
+
+    function store(uint256 _favNumber) public returns (uint256) {
         favNumber = _favNumber;
+        return _favNumber;
     }
-    
+
     // view, pure (needed if doing some math)
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return favNumber;
     }
-    
+
     function addPerson(string memory _name, uint256 _favNumber) public {
         people.push(People(_favNumber, _name));
         nameToFavNumber[_name] = _favNumber;
--- deploy.py
+++ deploy.py
@@ -2,7 +2,7 @@ from solcx import compile_standard, install_solc
 import json
 import os
 from web3 import Web3
-from dotenv import load_dotenv
+from dotenv import load_dotenv  # for private key (.env file)
 
 load_dotenv()
 
@@ -31,20 +31,31 @@ compiled_sol = compile_standard(
 with open("compiled_sol.json", "w") as file:
     json.dump(compiled_sol, file)
 
-# get bytecode
+# get bytecode from compile_sol.json
 bytecode = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["evm"][
     "bytecode"
 ]["object"]
 
-# get ABI
+# get ABI from compile_sol.json
 abi = compiled_sol["contracts"]["SimpleStorage.sol"]["SimpleStorage"]["abi"]
 
 # for connecting to ganache
-w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:7545"))
-chain_id = 1337
-my_address = "0xf53fEF0A054ade1020C72CfCf3bf06eAe23e86D9"
+# w3 = Web3(Web3.HTTPProvider("http://127.0.0.1:8545"))
+# chain_id = 1337
+# my_address = "0x90F8bf6A479f320ead074411a4B0e7944Ea8c9C1"
+# private_key = os.getenv("PRIVATE_KEY")
+# print(private_key)
+
+# for connecting to Rinkeby
+
+w3 = Web3(
+    Web3.HTTPProvider("https://rinkeby.infura.io/v3/d1dbebcb99594cd9900027838fdff0eb")
+)
+chain_id = 4
+my_address = "0x015Eb143584DfdD38D4605c73D601F41d02525Cf"  # get it from metamask
 private_key = os.getenv("PRIVATE_KEY")
 print(private_key)
+
 
 # Create contract in phyton
 SimpleStorage = w3.eth.contract(abi=abi, bytecode=bytecode)
@@ -58,4 +69,42 @@ transaction = SimpleStorage.constructor().buildTransaction(
 # Sign Transaction with private key
 signed_txn = w3.eth.account.sign_transaction(transaction, private_key=private_key)
 # Send Transaction
+print("Deploying contract...")
 tx_hash = w3.eth.send_raw_transaction(signed_txn.rawTransaction)
+# Transaction confirmation
+tx_receipt = w3.eth.wait_for_transaction_receipt(tx_hash)
+print("Deployed!")
+
+#################################################################
+# Working with contract
+
+# Contract address, Contract ABI
+simple_storage = w3.eth.contract(address=tx_receipt.contractAddress, abi=abi)
+# Call -> Simulate making a call and getting return value (no state change)
+# Transact -> Actually making a state change
+print(
+    # Initial value of favorite number
+    simple_storage.functions.retrieve().call()
+)  # use call because retrieve func is view
+
+# print(simple_storage.functions.store(15).call())
+print("Updating contract...")
+store_transaction = simple_storage.functions.store(15).buildTransaction(
+    {"chainId": chain_id, "from": my_address, "nonce": nonce + 1}
+)
+
+# sign txn
+signed_store_txn = w3.eth.account.sign_transaction(
+    store_transaction, private_key=private_key
+)
+# send txn
+send_store_tx = w3.eth.send_raw_transaction(signed_store_txn.rawTransaction)
+# txn confirmation
+tx_receipt = w3.eth.wait_for_transaction_receipt(send_store_tx)
+
+print("Updated!")
+
+print(
+    # Initial value of favorite number
+    simple_storage.functions.retrieve().call()
+)
